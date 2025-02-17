eee982d64e5482a84b58f4d8bdb3e2a327ab5c11
Muhammad Rifqi Rizqullah <rifqirizqullah7104@gmail.com>
2025-02-17 10:06:21 +0700

    Jobsheet 2 Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,38 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "NIM : 2341720091 <br>
+            Nama : Muhammad Rifqi Rizqullah";
+});
+
+Route::get('/hello', function () {
+    return "Hello World";
+});
+
+Route::get('/world', function () {
+    return "World";
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "Nama saya " . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return "Post ke-" . $postId . " Comment ke-" . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {    
+    return "Halaman Artikel dengan ID {$id}";
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return "Nama saya " . $name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return "Nama saya " . $name;
 });
